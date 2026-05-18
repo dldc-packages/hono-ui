8c2f469cded33b154f853d70160cea583f3b949f
Etienne Deladonchamps <e.deladonchamps@gmail.com>
2026-05-18 20:39:40 +0200

    Reorder arguments in resolveClassNames for improved clarity

--- src/utils/resolveClassNames.ts
+++ src/utils/resolveClassNames.ts
@@ -3,8 +3,8 @@ import type { ClassValue, CxArgs } from "./types.ts";
 
 export function resolveClassNames(classValue: ClassValue, ...args: CxArgs): string | Promise<string> | undefined {
   const arr = [
-    ...(Array.isArray(classValue) ? classValue : [classValue]),
     ...args,
+    ...(Array.isArray(classValue) ? classValue : [classValue]),
   ];
   if (arr.length === 0) {
     return undefined;
